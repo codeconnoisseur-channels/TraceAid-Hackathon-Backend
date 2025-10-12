34417290282993d6f6777f2fe42008dbcb33b490
codeconnoisseur-channels <channelsokunade@gmail.com>
2025-10-12 20:02:34 +0100

    added user model and updated server

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require("express")
-require("./config/database")
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const DB = process.env.MONGO_URI;
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log("Error connecting database", err.message);
+  });
 
-const port = process.env.PORT
-app.listen(port, ()=>{
-  console.log(`Server is listening on port: ${port}`)
-})
+const app = express();
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
