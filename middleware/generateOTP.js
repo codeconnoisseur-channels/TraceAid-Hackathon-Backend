48b0627074be98f581b6db4ca5f060d09dfe4ea6
codeconnoisseur-channels <channelsokunade@gmail.com>
2025-10-12 22:00:13 +0100

    otp

---
+++ middleware/generateOTP.js
@@ -0,0 +1,7 @@
+const generateOTPCode = () => {
+  const otp = Math.floor(1000 + Math.random() * 9000);
+  const expiresAt = Date.now() + 10 * 60 * 1000;
+  return { otp, expiresAt };
+};
+
+module.exports = generateOTPCode;
--- model/userModel.js
+++ model/userModel.js
@@ -13,10 +13,19 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
       type: String,
       required: true,
+    },
+    otp: {
+      type: String,
+    },
+    otpExpiresAt: {
+      type: Date,
     },
     isVerified: {
       type: Boolean,
