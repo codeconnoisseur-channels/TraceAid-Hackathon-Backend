59400179d54c34bde856d06500914e5c478dbece
codeconnoisseur-channels <channelsokunade@gmail.com>
2025-10-14 10:34:54 +0100

    update

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT || 7777;
 const mongoose = require("mongoose");
-const DB = process.env.MONGO_URI;
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log("Database connected successfully");
-  })
-  .catch((err) => {
-    console.log("Error connecting database", err.message);
-  });
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server is listening on port: ${port}`);
+app.use(express.json());
+app.use(cors());
+
+app.use((error, req, res, next) => {
+  if (error) {
+    res.status(500).json({
+      statusCode: false,
+      statusText: "Internal Server Error",
+      message: error.message,
+    });
+  }
+  next();
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting database", error.message);
+  });
