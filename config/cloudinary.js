a698c32d1aa0168ace20c66166e57d68ecb652ce
Lanreken <adelugbaolanrewaju@gmail.com>
2025-11-11 16:15:58 +0100

    adjusted the fundraiser dashboard

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,12 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 require('dotenv').config();
 
-console.log("CLOUDINARY_CONFIG:", {
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-});
-
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
