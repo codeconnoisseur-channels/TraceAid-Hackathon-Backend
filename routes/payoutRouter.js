702ddb1403de317b62d4e4eba2e5f0677a808e9d
Lanreken <adelugbaolanrewaju@gmail.com>
2025-11-14 15:58:57 +0100

    adjuste admin and added email templates

--- routes/payoutRouter.js
+++ routes/payoutRouter.js
@@ -3,7 +3,6 @@ const { protectAdmin, restrictAdmin } = require("../middleware/adminAuth");
 
 const router = require("express").Router();
 
-
 router.post("/create-payout", protectAdmin, restrictAdmin, createPayoutByAdmin)
 
 router.get("/get-pending-payout", protectAdmin, restrictAdmin, getPendingPayoutRequests)
